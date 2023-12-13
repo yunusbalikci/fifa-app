17a87eda2b0e0928ff97c74957d274b41801df8c
yunusbalikci <balikci.yunus06@gmail.com>
2023-12-13 15:27:51 +0300

    13.12

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -19,16 +19,25 @@ export default function UserHeader(){
     ]
     return(
         <div className="flex">
-            <div className={`${open ? "w-72" : "w-24 "} duration-300 h-screen p-5 pt-8 bg-fifa-blue relative`}>
-                <img src={control} alt="" className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-fifa-gray ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}></img>
+            <div className={`${open ? "w-72" : "w-24 "} duration-300 h-full p-5 pt-8 bg-fifa-blue relative`}>
+                <img src="https://cdn3.iconfinder.com/data/icons/cosmo-color-player-1/40/button_backward_1-512.png" alt="" className={`absolute cursor-pointer rounded-full -right-3 top-9 w-8 h-8 border-2 border-fifa-gray ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}></img>
                 <div className="flex gap-x-4 items-center">
                     <img className={`cursor-pointer duration-500 w-16 h-16 ${open && "rotate-[360deg]"}`} src={football}></img>
                     <h1 className={`font-open text-white origin-left text-xl ${!open && "hidden"} duration-300 `}>FCMS</h1>
+                   
                 </div>
+                <div className={`flex font-open space-x-4 mt-3  -mb-3 justify-center  text-white origin-left text-xl ${!open && "hidden"} duration-300 `}>
+                        <img className="h-8 w-8 rounded-full mt-1 items-end justify-end cursor-pointer hover:bg-fifa-gray duration-300" src="https://www.iconpacks.net/icons/1/free-mail-icon-142-thumb.png"></img>
+                        <img className="h-8 w-8 rounded-full mt-1 items-end justify-end cursor-pointer hover:bg-fifa-gray duration-300" src="https://cdn-icons-png.flaticon.com/512/3119/3119338.png"></img>
+                        <div className="flex space-x-2 hover:bg-fifa-gray duration-300 rounded-md">
+                           <img src="https://img.a.transfermarkt.technology/portrait/big/118-1682605652.jpg?lm=1" className="h-10 w-10 rounded-full"></img> 
+                           <p className="mt-1 text-lg">Klopp</p>
+                        </div>
+                    </div>
                 <ul className="pt-6">
                     {Menus.map((menu, index) => (
                         <li className={`text-gray-300 mt-3 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-gray-600 duration-200 rounded-md ${menu.gap ? "mt-9" : "mt-2"} `} key={index}>
-                            <img src={menu.src} className="w-8 h-8" />
+                            <img src={menu.src} className="item-center w-8 h-8" />
 
                             <span className={`${!open && "hidden"} origin-left text-xl duration-200`}>{menu.title}</span>
                         </li>
--- src/pages/private/Dashboard.js
+++ src/pages/private/Dashboard.js
@@ -1,23 +1,103 @@
 import React from "react"
 import UserHeader from "components/UserHeader"
-import Radar from "images/radar.png"
+import { motion } from "framer-motion";
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {
+      subject: 'Shooting',
+      A: 120,
+      B: 110,
+      fullMark: 90,
+    },
+    {
+      subject: 'Mental',
+      A: 98,
+      B: 130,
+      fullMark: 91,
+    },
+    {
+      subject: 'Passing',
+      A: 86,
+      B: 130,
+      fullMark: 76,
+    },
+    {
+      subject: 'Ball Skills',
+      A: 99,
+      B: 100,
+      fullMark: 85,
+    },
+    {
+      subject: 'Physics',
+      A: 85,
+      B: 90,
+      fullMark: 150,
+    },
+    {
+      subject: 'Vision',
+      A: 65,
+      B: 85,
+      fullMark: 83,
+    },
+  ];
+
+  
+
+
 export default function Dashboard(){
+
+    const getBackgroundColor = ({ value }) => {
+        if (value < 65){
+            return "bg-red-500"
+        } else if (value < 75){
+            return "bg-orange-500"
+        }
+        else{
+            return "bg-green-500"
+        }
+        }
+    
+
     return(
-        <div className="bg-fifa-gray">
+        <motion.div
+        initial = {{ opacity: 0,}}
+        animate = {{ opacity:1}}
+
+        className="bg-zinc-100">
             <div className="flex">
-            <UserHeader></UserHeader>
-            <div className="grid grid-cols-2 w-full gap-4  bg-fifa-bg gap-4 font-open text-black py-5 pl-3 font-bold text-2xl">
+            <UserHeader>
+            </UserHeader>
+            <div className="grid grid-cols-2 w-full gap-4  bg-zinc-100 gap-4 font-open text-black py-5 pl-3  text-2xl">
                 <div>
+                    <input placeholder="Search..." className="w-full mb-3 text-xl pl-1 ml-1 font-open border border-gray-300 rounded-lg"></input>
                     <h1>Prediction Player Performance -Player Name-</h1>
                     <div className="flex item-center mx-auto justify-center">
-                        <img className="flex item-center mt-10 ml-3 mb-5 w-96 h-96" src="https://pngimg.com/d/football_player_PNG134.png"></img>
+                        <img className="flex item-center mt-10 ml-3 mb-5 w-80 h-80" src="https://pngimg.com/d/football_player_PNG134.png"></img>
                     </div>
-                    <div className="box-border ml-5 mr-5 mt-1 rounded my-auto shadow-lg h-52 w-auto p-4 bg-white hover:bg-gray-100 transition duration-200">
+                    <div className="box-border ml-5 mr-5 mt-1 rounded my-auto shadow-lg h-96 w-auto p-4 bg-white hover:bg-gray-100 transition duration-200">
                         <h1 className="text-center text-lg">View Predictions</h1>
-                        <img className="w-80  mx-auto" src={Radar}></img>
-                    </div>
+                        <ResponsiveContainer width="100%" height="100%">
+                            <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
+                            <PolarGrid />
+                            <PolarAngleAxis dataKey="subject" />
+                            <PolarRadiusAxis />
+                            <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+                            </RadarChart>
+                        </ResponsiveContainer>                    
+      </div>
                 </div>
                 <div className="ml-1 mr-3">
+                    <div className="flex item-end mx-auto justify-end space-x-4 ">
+                        <img className="h-8 w-8 rounded-full mt-1 items-end justify-end cursor-pointer hover:bg-fifa-gray duration-300" src="https://www.iconpacks.net/icons/1/free-mail-icon-142-thumb.png"></img>
+                        <img className="h-8 w-8 rounded-full mt-1 items-end justify-end cursor-pointer hover:bg-fifa-gray duration-300" src="https://cdn-icons-png.flaticon.com/512/3119/3119338.png"></img>
+                        <div className="flex space-x-2 hover:bg-fifa-gray duration-300 rounded-md">
+                           <img src="https://img.a.transfermarkt.technology/portrait/big/118-1682605652.jpg?lm=1" className="h-10 w-10 rounded-full"></img> 
+                           <p className="mt-1 text-lg">Klopp</p>
+                        </div>
+                        <img className="h-8 w-8 rounded-full items-end mt-1 justify-end cursor-pointer hover:bg-fifa-gray duration-300" src="https://cdn1.iconfinder.com/data/icons/heroicons-ui/24/logout-512.png"></img>
+
+                    </div>
                     <h1 className="text-center">Administration</h1>
                     <table className="table-auto bg-white w-full h-auto rounded-md shadow-md uppercase font-open border text-xl mt-3">
                         <thead>
@@ -29,35 +109,32 @@ export default function Dashboard(){
                         <tbody className="border">
                             <tr>
                                 <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">add</td>
-                                <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">90</td>
+                                <td className={`pl-2  duration-200 rounded-md ${getBackgroundColor({ value: 65 })}`}>{65}</td>
                             </tr>
                             <tr>
                                 <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">add</td>
-                                <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">90</td>
+                                <td className={`pl-2  duration-200 rounded-md ${getBackgroundColor({ value: 85 })}`}>{85}</td>
                             </tr>
                             <tr>
                                 <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">add</td>
-                                <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">90</td>
+                                <td className={`pl-2  hover:opacity-70 duration-200 rounded-md ${getBackgroundColor({ value: 80 })}`}>{80}</td>
                             </tr>
                             <tr>
                                 <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">add</td>
-                                <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">90</td>
+                                <td className={`pl-2  duration-200 rounded-md ${getBackgroundColor({ value: 60 })}`}>{60}</td>
                             </tr>
                             <tr>
                                 <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">add</td>
-                                <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">90</td>
+                                <td className={`pl-2  duration-200 rounded-md ${getBackgroundColor({ value: 65 })}`}>{65}</td>
                             </tr>
                             <tr>
                                 <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">add</td>
-                                <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">90</td>
+                                <td className={`pl-2  duration-200 rounded-md ${getBackgroundColor({ value: 90 })}`}>{90}</td>
                             </tr>
-                            <tr>
-                                <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">add</td>
-                                <td className="pl-2 hover:bg-gray-100 duration-200 rounded-md">90</td>
-                            </tr>
+                          
                         </tbody>
                     </table>
-                    <div className="w-full bg-white rounded-md shadow-md mt-3">
+                    <div className="w-full bg-white rounded-md shadow-md mt-10">
                         <h1 className="text-center">ABILITIES GROWTH</h1>
                         <img src="https://miro.medium.com/v2/resize:fit:1942/1*le8ngJPQRImQiPL9gQR-WQ.png"></img>
 
@@ -65,6 +142,6 @@ export default function Dashboard(){
                 </div>
             </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
