d6b9539c919da4754c5a796290423ab0cdf443a5
Hasan Basri Akcan <akcanhasanbasri@hotmail.com>
2023-12-16 17:33:11 +0300

    Add admin models and register them in admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Players20, TeamsAndLeagues
 
 # Register your models here.
+
+
+@admin.register(Players20)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = [
+        "age",
+        "short_name",
+        "club",
+        "overall",
+        "value_eur",
+        "wage_eur",
+        "player_positions",
+    ]
+    list_filter = ["club", "overall", "player_positions"]
+    search_fields = ["short_name", "club", "player_positions"]
+    ordering = ["overall"]
+    list_per_page = 20
+
+    class Meta:
+        model = Players20
+
+
+@admin.register(TeamsAndLeagues)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ["url", "league_name"]
+    list_filter = ["league_name"]
+    search_fields = ["league_name"]
+    ordering = ["league_name"]
+    list_per_page = 20
+
+    class Meta:
+        model = TeamsAndLeagues
