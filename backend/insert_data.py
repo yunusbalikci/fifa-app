cd5e4ffb6c3613bdb0f721690c0cb10551b587d3
Hasan Basri Akcan <akcanhasanbasri@hotmail.com>
2023-12-16 16:19:58 +0300

    player_20 and teams_and_leagues data inserted into sqlite3 db

--- backend/insert_data.py
+++ backend/insert_data.py
@@ -1,11 +1,1 @@
-import pandas as pd
-
-
-pd.set_option("display.max_columns", None)
-pd.set_option("display.max_rows", None)
-pd.set_option("display.width", None)
-pd.set_option("display.float_format", lambda x: "%.3f" % x)
-
-df = pd.read_csv("./data/teams_and_leagues.csv")
-
-df.dtypes
+print("denemeeeeee")
