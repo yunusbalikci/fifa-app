a1414f74745997bc1b260ace7b2860affeb7e3ef
Hasan Basri Akcan <akcanhasanbasri@hotmail.com>
2023-12-30 17:53:36 +0300

    Add PlayerValue API endpoint for predicting player value

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("players/<int:pk>/", views.PlayerDetail, name="player"),
     path("teams/", views.TeamList, name="teams"),
     path("deneme/", views.deneme, name="deneme"),
+    path("PlayerValue/", views.PlayerValue, name="PlayerValue"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -4,6 +4,14 @@ from rest_framework.decorators import api_view
 from .models import Players20, TeamsAndLeagues
 from .serializers import Players20Serializer, TeamsAndLeaguesSerializer
 from django.http import HttpResponse
+from joblib import load
+import numpy as np 
+import os
+import pandas as pd
+
+model = load(os.path.join(os.path.dirname(__file__), "../Models/priceQuess.pkl"))
+
+
 
 # Create your views here.
 
@@ -69,3 +77,54 @@ def deneme(request):
         },
     ]
     return Response(routes)
+
+@api_view(["GET", "POST"])
+def PlayerValue(request):
+    if request.method == "POST":
+        Overall = request.data.get("Overall")
+        Potential = request.data.get("Potential")
+        Ball_control = request.data.get("Ball_control")
+        Composure = request.data.get("Composure")
+        Reactions = request.data.get("Reactions")
+        Age = request.data.get("Age")
+
+        # Create a list of features
+        features = [
+            Overall,
+            Potential,
+            Ball_control,
+            Composure,
+            Reactions,
+            Age,
+        ]
+
+        # Create a dictionary for the response
+        response = {}
+
+        # Create a list of feature names
+        feature_names = [
+            "Overall",
+            "Potential",
+            "Ball control",
+            "Composure",
+            "Reactions",
+            "Age",
+        ]
+
+        # Create a dictionary for the features
+        features_dict = dict(zip(feature_names, features))
+
+        # Create a dataframe from the features
+        features_df = pd.DataFrame([features_dict])
+
+        # Make a prediction using the random forest model
+        prediction = model.predict(features_df)
+
+        # Add the prediction to the response dictionary
+        response["prediction"] = prediction[0]
+
+        # Send the response dictionary as a json object
+        return Response(response)
+
+    else:
+        return Response("Send a POST request with the features to get a prediction.")
